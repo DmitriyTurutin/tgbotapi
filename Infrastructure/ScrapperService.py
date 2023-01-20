166c796d8fefab447cb15987380c2a93e665e4d5
Dima Turutin <qwe068776@gmail.com>
2023-01-20 20:04:13 +0300

    Try to fix requests

--- Infrastructure/ScrapperService.py
+++ Infrastructure/ScrapperService.py
@@ -4,9 +4,74 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from datetime import datetime
+import requests
 import locale
 import re
+# From test request
+##############################################
+##############################################
+##############################################
+##############################################
+# from selenium import webdriver
+# import time
+# import json
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# import requests
 
+# driver = webdriver.Firefox()
+
+# email = "aleksandrdonskov@gmail.com" 
+# password = "hpkorolev2020"
+
+# driver.get("https://korolev.hookah.work/")
+# email_input = WebDriverWait(driver, 10).until(
+#     EC.presence_of_element_located((By.ID, 'loginform-email'))
+# )
+# password_input = driver.find_element(By.ID, 'loginform-password')
+# email_input.send_keys(email)
+# password_input.send_keys(password)
+
+# submit = driver.find_element(By.CLASS_NAME, 'btn-primary')
+# submit.click()
+
+# time.sleep(1)
+# cookies = driver.get_cookies()
+
+
+# session = requests.Session()
+# session.headers.update({'User-Agent': driver.execute_script("return navigator.userAgent;")})
+# driver.quit()
+# for cookie in cookies:
+#     session.cookies.update({cookie['name']: cookie['value']})
+
+# import requests
+# session = requests.Session()
+
+# params = {'iDisplayStart': 0, 'iDisplayLength': 6500, 'mDataProp_12': 'created_at', 'bSortable_12': 'true', 'iSortCol_0': 12, 'sSortDir_0': 'desc', 'iSortingCols': 1}
+# response = session.get("https://korolev.hookah.work/sale/data", params=params)
+
+# response = session.get("https://korolev.hookah.work/sale/data?iDisplayStart=0&iDisplayLength=6500&mDataProp_12=created_at&bSortable_12=true&iSortCol_0=12&sSortDir_0=desc&iSortingCols=1")
+# response = session.get("https://korolev.hookah.work/sale/data?iDisplayStart=0&iDisplayLength=6500&mDataProp_12=created_at&bSortable_12=true&iSortCol_0=12&sSortDir_0=desc&iSortingCols=1")
+# response = session.get("https://korolev.hookah.work/sale/data?iDisplayStart=0&iDisplayLength=6500&mDataProp_12=created_at&bSortable_12=true&iSortCol_0=12&sSortDir_0=desc&iSortingCols=1")
+# response = session.get("https://korolev.hookah.work/sale/data?iDisplayStart=0&iDisplayLength=6500&mDataProp_12=created_at&bSortable_12=true&iSortCol_0=12&sSortDir_0=desc&iSortingCols=1")
+# response = session.get("https://korolev.hookah.work/sale/data?iDisplayStart=0&iDisplayLength=6500&mDataProp_12=created_at&bSortable_12=true&iSortCol_0=12&sSortDir_0=desc&iSortingCols=1")
+# response = session.get("https://korolev.hookah.work/sale/data?iDisplayStart=0&iDisplayLength=6500&mDataProp_12=created_at&bSortable_12=true&iSortCol_0=12&sSortDir_0=desc&iSortingCols=1")
+# response = session.get("https://korolev.hookah.work/sale/data?iDisplayStart=0&iDisplayLength=6500&mDataProp_12=created_at&bSortable_12=true&iSortCol_0=12&sSortDir_0=desc&iSortingCols=1")
+# data = response.json()
+
+
+
+# sales = data['data']
+# sales[0]['total']
+# sales[0]['client_id']
+
+# From test request
+##############################################
+##############################################
+##############################################
+##############################################
 class Sale: 
     title: str
     price: str
@@ -26,11 +91,7 @@ class Scrapper:
         self.driver = webdriver.Firefox()
 
     def scan_first_page(self) -> list:
-        self.driver.get(self.url)
-
-        self.html = []
-
-        # Wait for the email input element to be present
+        self.driver.get("https://korolev.hookah.work/")
         email_input = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.ID, 'loginform-email'))
         )
@@ -41,34 +102,34 @@ class Scrapper:
         submit = self.driver.find_element(By.CLASS_NAME, 'btn-primary')
         submit.click()
 
-        # Wait for the "Продажи" link to be present 
-        sales = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.LINK_TEXT, 'Продажи'))
-        )
-        sales.click()
+        # time.sleep(1)
+        cookies = self.driver.get_cookies()
 
 
-        # Wait for the page to be fully loaded
-        WebDriverWait(self.driver, 10).until(EC.title_is(self.driver.title))
-        list = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, 'select.form-control'))
-        )
-
-        list.click()
-        option = self.driver.find_element(By.XPATH, ".//option[text()='100']")
-
-        option.click()
-
-        self.html.append(self.driver.page_source)
-
+        session = requests.Session()
+        session.headers.update({'User-Agent': self.driver.execute_script("return navigator.userAgent;")})
         self.driver.quit()
+        for cookie in cookies:
+            session.cookies.update({cookie['name']: cookie['value']})
 
-        self.clean_data(self.html)
 
-        return self.data
+        number_of_database_elements = session.get("https://korolev.hookah.work")
+        iterations = number_of_database_elements.json()
+        # FIXME: Change to proper name
+        itreations = iterations['iNumberOfElements'] // 6500
+        i = 1
+
+        response = None
+        while i < itreations:
+            params = {'iDisplayStart': i, 'iDisplayLength': iterations, 'mDataProp_12': 'created_at', 'bSortable_12': 'true', 'iSortCol_0': 12, 'sSortDir_0': 'desc', 'iSortingCols': 1}
+            response += session.get("https://korolev.hookah.work/sale/data", params=params)
+            i *= 6500 
+
+        dict = response.json()
+         
 
     
-    def scan(self) -> list:
+    def scan(self) -> dict:
         self.driver.get(self.url)
 
         self.html = []
